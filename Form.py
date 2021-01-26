dfce56dffe7ed5c75acaef7a6f91b236be168aa7
Tuf <galaktia@live.ru>
2021-01-27 01:32:06 +0500

    Вторая часть

--- Form.py
+++ Form.py
@@ -25,14 +25,14 @@ try:
     browser = webdriver.Chrome()
     browser.get(link)
 
-    input1 = browser.find_element_by_tag_name("input")
-    input1.send_keys("Ivan")
-    input2 = browser.find_element_by_name("last_name")
-    input2.send_keys("Petrov")
-    input3 = browser.find_element_by_class_name("form-control.city")
-    input3.send_keys("Smolensk")
-    input4 = browser.find_element_by_id("country")
-    input4.send_keys("Russia")
+    img = browser.find_element_by_id("treasure")
+    img_checked = img.get_attribute("valuex=''")
+    print(valeimg_checked
+    print("value of people radio: ", people_checked)
+    assert people_checked is not None, "People radio is not selected by default"
+    
+
+
     button = browser.find_element_by_css_selector("button.btn")
     button.click()
 
